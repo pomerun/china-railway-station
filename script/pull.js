34d87951c9b1cc86c70bb918b017e7aa00138e8f
pecasha <chihai@pecasha.com>
2026-02-02 12:02:51 +0800

    :safety_vest: 添加拉取数据CDN鉴权

--- script/pull.js
+++ script/pull.js
@@ -2,8 +2,11 @@
 import fs from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
+import crypto from "node:crypto";
 
-const data = await fetch("https://cdn.pome.run/station_name.js");
+const t = Math.floor(Date.now() / 1000);
+const s = crypto.createHash("sha256").update(`${process.env.CDN_AUTH_KEY}/station_name.js${t}`, "utf8").digest("hex");
+const data = await fetch(`https://cdn.pome.run/station_name.js?s=${s}&t=${t}`);
 let dataStr = (await data.text()).match(/'([^']*)'/)[1];
 dataStr = dataStr.slice(1);
 dataStr = dataStr.replaceAll("|||", "");
